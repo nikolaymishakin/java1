7b8c76053a121e805227e32c1586ceb0032f272d
Николай <nikolaymishakin@yandex.ru>
2019-11-04 18:27:22 +0300

    Revert "Hello World Start"
    This reverts commit 1389090d

--- src/ru/nik/mish/HelloWorld.java
+++ src/ru/nik/mish/HelloWorld.java
@@ -7,9 +7,6 @@ public class HelloWorld {
     static int addition (int x, int y) {
         return x+y ;
     }
-    static int addition1 (int x, int y) {
-        return x+y ;
-    }
    public static void main (String [] args) {
         println("Привет мир!");
         println("Мир, ты слышишь меня?");
