8182855871c090b422629d5aedcd44f8b8414a9b
Николай <nikolaymishakin@yandex.ru>
2019-11-04 17:49:50 +0300

    Hello World Start

---
+++ src/ru/nik/mish/HelloWorld.java
@@ -0,0 +1,17 @@
+package ru.nik.mish;
+
+public class HelloWorld {
+    static void println(String Str) {
+        System.out.println(Str);
+    }
+    static int addition (int x, int y) {
+        return x+y ;
+    }
+   public static void main (String [] args) {
+        println("Привет мир!");
+        println("Мир, ты слышишь меня?");
+        System.out.print("Я знаю что 5 + 7 =");
+        System.out.print(addition(5,7));
+
+    }
+}
