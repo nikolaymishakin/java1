0462a5e47aa555337fbd5dfbd14d6340bcce67e5
Николай <nikolaymishakin@yandex.ru>
2019-11-09 00:01:22 +0300

    ru.progwards.java1.lessons.helloworld

--- src/ru/progwards/java1/lessons/helloworld/Task2.java
+++ src/ru/progwards/java1/lessons/helloworld/Task2.java
@@ -1,16 +1,14 @@
 package ru.progwards.java1.lessons.helloworld;
 
 public class Task2 {
-    public static int subtraction (int x, int y){
+    public static void subtraction (int x, int y){
             int z=x-y;
-            return z;
-    }
-    public static void main (String[] args){
-
         System.out.println("Вызвана функция subtraction() с параметрами x =89,y=55");
         System.out.print("Результат z=");
-        int z = subtraction(89,55);
         System.out.print(z);
+    }
+    public static void main (String[] args){
+        subtraction( 89,55);
     }
 }
 
