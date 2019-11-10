e520a8b8945ebf724fc1fc91bda35fa8d19801b2
Николай <nikolaymishakin@yandex.ru>
2019-11-10 22:00:19 +0300

    ru.progwards.java1.lessons.helloworld

---
+++ src/ru.progwards.java1.lessons.basics/Astronomy.java
@@ -0,0 +1,33 @@
+package ru.progwards.java1.lessons.basics;
+
+public class Astronomy {
+    public static Double sphereSquare(Double r){
+        double S;
+        final double pi=3.14;
+        S=4*pi*r*r;
+        return S;
+    }
+    public static Double earthSquare(){
+        double e=sphereSquare(6371.2);
+        return e;
+    }
+    public static Double mercurySquare(){
+        double m=sphereSquare(2439.7);
+        return m;
+    }
+    public static Double jupiterSquare(){
+        double j=sphereSquare(71492.);
+        return j;
+    }
+    public static Double earthVsMercury(){
+        double e= earthSquare(),m=mercurySquare(),em=e/m;
+        return em;
+    }
+    public static Double earthVsJupiter(){
+        double e=earthSquare(),j=jupiterSquare(),ej=e/j;
+        return ej;
+    }
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ru.progwards.java1.lessons.basics/ReverseDigits.java
@@ -0,0 +1,23 @@
+package ru.progwards.java1.lessons.basics;
+
+public class ReverseDigits {
+    public static int reverseDigits(int number){
+        int num=number%10;
+        String int1=Integer.toString(num);
+        int num1=number/10%10;
+        String int2=Integer.toString(num1);
+        int num2=number/100;
+        String int3=Integer.toString(num2);
+        String int4=int1+int2+int3;
+        int num5=Integer.parseInt(int4);
+        return num5;
+
+    }
+
+    public static void main(String[] args) {
+       int num=reverseDigits(123);
+        System.out.println(num);
+
+    }
+
+}
