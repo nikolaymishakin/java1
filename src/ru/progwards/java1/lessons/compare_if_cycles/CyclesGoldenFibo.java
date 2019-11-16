befe3f325989f10d5037dabfb88a5652f20b28d8
Николай <nikolaymishakin@yandex.ru>
2019-11-17 00:37:51 +0300

    ru.progwards.java1.lessons.basics

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -15,13 +15,22 @@ public class CyclesGoldenFibo {
         return curr;
     }
     public static boolean isGoldenTriangle(int a,int b,int c){
-        if(a==b||a==c||b==c)
-            if()
+        if(a==b)
+            if(a/c>=1.61703&&a/c<=1.61903) return true;
+            else return false;
+            if(a==c)
+                if(a/b>=1.61703&&a/b<=1.61903) return true;
+                else return false;
+                if(b==c)
+                    if(b/a>=1.61703&&b/a<=1.61903) return true;
+                    else return false;
+                    else return false;
+
     }
 
     public static void main(String[] args) {
         int curr=1,next=1,prev=0;
-        for(int i=0;i<10;i++){
+        for(int i=0;i<15;i++){
             curr=next+prev;
             next=prev;
             prev=curr;
