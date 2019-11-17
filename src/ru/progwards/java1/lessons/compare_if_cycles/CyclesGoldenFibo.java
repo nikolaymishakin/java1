9a7012c92e70714ef5584eb1fc8bcfbf1539c422
Николай <nikolaymishakin@yandex.ru>
2019-11-17 14:55:51 +0300

    ru.progwards.java1.lessons.basics

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -2,7 +2,8 @@ package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class CyclesGoldenFibo {
     public static boolean containsDigit(int number, int digit){
-        if(number%10==digit||number%1==digit) return true;
+        int num=number/10;
+        if(number%10==digit||num%10==digit) return true;
         else return false;
     }
     public static int fiboNumber(int n){
@@ -15,28 +16,27 @@ public class CyclesGoldenFibo {
         return curr;
     }
     public static boolean isGoldenTriangle(int a,int b,int c){
+        double d=a/c,e=a/b,f=b/a;
         if(a==b)
-            if(a/c>=1.61703&&a/c<=1.61903) return true;
-            else return false;
-            if(a==c)
-                if(a/b>=1.61703&&a/b<=1.61903) return true;
-                else return false;
-                if(b==c)
-                    if(b/a>=1.61703&&b/a<=1.61903) return true;
-                    else return false;
+        {if(d>=1.61703&&d<=1.61903) return true; else return false;}
+            else if(a==c)
+            {if(e>=1.61703&&e<=1.61903) return true; else return false;}
+                else if(b==c) { if(f>=1.61703&&f<=1.61903) return true; else return false;}
                     else return false;
 
     }
 
     public static void main(String[] args) {
         int curr=1,next=1,prev=0;
-        for(int i=0;i<15;i++){
-            curr=next+prev;
-            next=prev;
-            prev=curr;
-            System.out.println(curr);
+        for(int i=0;i<15;i++) {
+            curr = next + prev;
+            next = prev;
+            prev = curr;
 
         }
+            boolean a=isGoldenTriangle(2,5,5);
+            System.out.println(a);
+
 
 
     }
