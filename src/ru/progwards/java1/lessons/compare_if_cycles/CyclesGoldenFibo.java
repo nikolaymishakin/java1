68b32d19f505099e2a73d4c293edd6ae1a622d4a
Николай <nikolaymishakin@yandex.ru>
2019-11-17 15:02:33 +0300

    ru.progwards.java1.lessons.basics

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -2,7 +2,7 @@ package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class CyclesGoldenFibo {
     public static boolean containsDigit(int number, int digit){
-        int num=number/10;
+        float num=number/10;
         if(number%10==digit||num%10==digit) return true;
         else return false;
     }
