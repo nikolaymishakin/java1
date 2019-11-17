f17cf7ecdd54882308b3077f2c7a733fd5460c41
Николай <nikolaymishakin@yandex.ru>
2019-11-17 15:53:27 +0300

    ru.progwards.java1.lessons.basics

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -2,8 +2,7 @@ package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class CyclesGoldenFibo {
     public static boolean containsDigit(int number, int digit){
-        double num=number%10;
-        if(num==(double)digit||number/10==digit) return true;
+        if(number%10==digit|number/10==digit) return true;
         else return false;
     }
     public static int fiboNumber(int n){
@@ -34,7 +33,7 @@ public class CyclesGoldenFibo {
             prev = curr;
 
         }
-            boolean a=containsDigit(28,5);
+            boolean a=containsDigit(28,8);
             System.out.println(a);
 
 
