287564a29643815a419297b048732fe6cdac24df
Николай <nikolaymishakin@yandex.ru>
2019-11-24 14:15:47 +0300

    ru.progwards.java1.lessons.basics

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -1,37 +1,42 @@
 package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class CyclesGoldenFibo {
-    public static boolean containsDigit(int number, int digit){
-        if((double)number%10==digit|number/10==digit) return true;
-        else return false;
+    public static boolean containsDigit(int number, int digit) {
+        int i = 0, num = number,num1=num%10;
+        if(num1==digit) return true;
+         for(;i<5;i++){
+            num/=10;
+            num1=num%10;
+            if(num1==digit) return true;
+        }
+
+        return false;
     }
     public static int fiboNumber(int n){
-        int i,next=1, prev=1, curr=0;
-        for(i=0;i<=n;i++){
+        int i,curr = 0,next=1,prev=1;
+        System.out.println(next);
+        System.out.println(prev);
+        for(i=2;i<n;i++){
             curr=next+prev;
             next=prev;
             prev=curr;
+           System.out.println(curr);
         }
         return curr;
     }
     public static boolean isGoldenTriangle(int a,int b,int c){
-        double d=a/c,e=a/b,f=b/a;
-        if(a==b)
-        {if(d>=1.61703&&d<=1.61903) return true; else return false;}
-            else if(a==c)
-            {if(e>=1.61703&&e<=1.61903) return true; else return false;}
-                else if(b==c) { if(f>=1.61703&&f<=1.61903) return true; else return false;}
-                    else return false;
-
+        double d=(double)a/c,e=(double)a/b,f=(double)b/a;
+        if(a==b&&d>=1.61703&&d<=1.61903)return true;
+        else if(a==c&&e>=1.61703&&e<=1.61903) return true;
+        else if(b==c&&f>=1.61703&&f<=1.61903)  return true;
+        else return false;
     }
 
     public static void main(String[] args) {
-        int n,a,curr,next=1,prev=1;
-        for(int i = 2; i<10; i++) {
-            curr=prev+next;
-            prev=next;
-            next=curr;
-            System.out.println(curr);
+        int a=fiboNumber(15);
+        boolean b=isGoldenTriangle(7000,7000,4325);
+        System.out.println(b);
         }
     }
-}
+
+
--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
@@ -2,13 +2,13 @@ package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class TriangleSimpleInfo {
     public static int maxSide(int a,int b,int c){
-        if(a>b&&a>c) return a;
-        if(b>a&&b>c) return b;
+        if(a>=b&&a>=c) return a;
+        if(b>=a&&b>=c) return b;
         else return c;
     }
     public static int minSide(int a,int b,int c){
-        if(a<b&&a<c) return a;
-        if(b<a&&b<c) return b;
+        if(a<=b&&a<=c) return a;
+        if(b<=a&&b<=c) return b;
         else return c;
     }
     public static boolean isEquilateralTriangle(int a,int b,int c){
@@ -17,6 +17,7 @@ public class TriangleSimpleInfo {
     }
 
     public static void main(String[] args) {
-
+        int c=minSide(5,5,5);
+        System.out.println(c);
     }
 }
