27249812394960fd19622015fce1f0cafbe5a87b
Николай <nikolaymishakin@yandex.ru>
2019-11-26 00:34:00 +0300

    ru.progwards.java1.lessons.basics

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -15,15 +15,14 @@ public class CyclesGoldenFibo {
 
     public static int fiboNumber(int n) {
         int i, c=0 , a=0, b=0 ;
-        if (c == 0 ) { a=1; b=1;
-            System.out.print(a);
-            System.out.print(b);
-        }
-        for (i = 0; i < n; i++) {
+        for (i = 1; i < n; i++) {
+            if (c == 0 ) { a=1; b=1;
+                System.out.println(a);
+                System.out.println(b); }
             c = a + b;
             a = b;
             b = c;
-            System.out.print(c);
+            System.out.println(c);
         }
         return c;
     }
@@ -37,8 +36,6 @@ public class CyclesGoldenFibo {
     }
 
     public static void main(String[] args) {
-        //boolean a=containsDigit(288,2);
-        //System.out.println(a);
        int b=fiboNumber(15);
     }
 }
