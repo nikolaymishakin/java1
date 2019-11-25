cf427446d2bd6943c9e7320de7c850dfece0daf5
Николай <nikolaymishakin@yandex.ru>
2019-11-25 23:40:15 +0300

    ru.progwards.java1.lessons.basics

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -2,40 +2,44 @@ package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class CyclesGoldenFibo {
     public static boolean containsDigit(int number, int digit) {
-        int i = 0, num = number,num1=0;
-        if(number%10==digit) return true;
-         for(;i<5;i++){
-            num/=10;
-            num1=num%10;
-            if(num1==digit) return true;
+        int i = 0, num = number, num1 = 0;
+        if (number % 10 == digit) return true;
+        for (; i < 5; i++) {
+            num /= 10;
+            num1 = num % 10;
+            if (num1 == digit) return true;
         }
 
         return false;
     }
-    public static int fiboNumber(int n){
-        int i,c=0,a=1,b=1;
-        System.out.print(a);
-        System.out.print(b);
-        for(i=2;i<n;i++){
-            c=a+b;
-            a=b;
-            b=c;
-           System.out.print(c);
+
+    public static int fiboNumber(int n) {
+        int i, c = 0, a = 1, b = 1;
+        if (a == 1 && b == 1) {
+            System.out.print(a);
+            System.out.print(b);
+        }
+        for (i = 0; i < n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+            System.out.print(c);
         }
         return c;
     }
-    public static boolean isGoldenTriangle(int a,int b,int c){
-        double d=(double)a/c,e=(double)a/b,f=(double)b/a;
-        if(a==b&&d>=1.61703&&d<=1.61903)return true;
-        else if(a==c&&e>=1.61703&&e<=1.61903) return true;
-        else if(b==c&&f>=1.61703&&f<=1.61903)  return true;
+
+    public static boolean isGoldenTriangle(int a, int b, int c) {
+        double d = (double) a / c, e = (double) a / b, f = (double) b / a;
+        if (a == b && d >= 1.61703 && d <= 1.61903) return true;
+        else if (a == c && e >= 1.61703 && e <= 1.61903) return true;
+        else if (b == c && f >= 1.61703 && f <= 1.61903) return true;
         else return false;
     }
 
     public static void main(String[] args) {
-        boolean a=containsDigit(287,1);
-        System.out.println(a);
-        }
+        int a = fiboNumber(5);
+
     }
+}
 
 
