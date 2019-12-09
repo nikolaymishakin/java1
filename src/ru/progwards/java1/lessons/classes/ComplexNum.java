0b94a3fbd796a8390dd5b5f26bae793558851ea6
Николай <nikolaymishakin@yandex.ru>
2019-12-10 00:56:39 +0300

    ru.progwards.java1.lessons.basics

--- src/ru/progwards/java1/lessons/classes/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes/ComplexNum.java
@@ -1,6 +1,6 @@
 package ru.progwards.java1.lessons.classes;
 public class ComplexNum {
-    public int a,b,c,d;;
+    public int a,b;
 
     public ComplexNum(int a,int b){
         this.a=a;this.b=b;
@@ -11,18 +11,18 @@ public class ComplexNum {
         return a+"+"+b+"i";
     }
      public ComplexNum add(ComplexNum num){
-        this.a=num.a;this.b=num.b;this.c=num.c;this.d=num.d;
-        return new ComplexNum(num.a+num.c,num.b+num.d);
+        return new ComplexNum(this.a+num.a,this.b+num.b);
     }
     public ComplexNum sub(ComplexNum num){
-        return new ComplexNum(num.a-num.c,num.c-num.d);
+        return new ComplexNum(this.a-num.a,this.b-num.b);
     }
     public ComplexNum mul(ComplexNum num){
-        return new ComplexNum(num.a*num.c-num.b*num.d,num.b*num.c+num.a*num.d);
+
+        return new ComplexNum(this.a*num.a-this.b*num.b,this.b*num.a+this.a*num.b);
     }
     public ComplexNum div(ComplexNum num){
-        int div= num.c*num.c+num.d*num.d;
-        return new ComplexNum((num.a*num.c+num.b*num.d)/div,(num.b*num.c-num.a*num.d)/div);
+        int div= num.a*num.a+num.b*num.b;
+        return new ComplexNum((this.a*num.a+this.b*num.b)/div,(this.b*num.a-this.a*num.b)/div);
     }
     public static void main(String[] args) {
         System.out.println();
