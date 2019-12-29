cb3c441bf4a4e31d16ba0eccefe46f3e4f268c86
Николай <nikolaymishakin@yandex.ru>
2019-12-29 17:57:12 +0300

    ru.progwards.java1.lessons.basics

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -14,12 +14,11 @@ public class CyclesGoldenFibo {
     }
 
     public static int fiboNumber(int n) {
-        int i=1, c=0 , a=0, b=0 ;
-        if(a==0)  b = 1;
-        for (; i <=n; i++) {
+        int i = 1, c = 0, a = 0, b = 1;
+        for (; i <= n; i++) {
             a = b;
             b = c;
-            c=a+b;
+            c = a + b;
         }
         return c;
     }
@@ -33,23 +32,22 @@ public class CyclesGoldenFibo {
     }
 
     public static void main(String[] args) {
-        int i,j,k;
+        int i, j, k;
 //        for (i = 1; i <= 15; i++) {
 //            int b = fiboNumber(i);
 //            System.out.println(b);
 //        }
-        for(i=1;i<=100;i++){
-            for(j=1;j<100;j++){
-                for(k=1;k<100;k++){
-                    if(isGoldenTriangle(i,j,k)==true){
-                        System.out.print(i);
-                        System.out.print(j);
+        for (i = 1; i <= 100; i++) {
+            for (j = 1; j <= 100; j++) {
+                for (k = 1; k <= 100; k++) {
+                    if (isGoldenTriangle(i, j, k) == true) {
+                        System.out.println(i);
+                        System.out.println(j);
                         System.out.println(k);
                     }
                 }
             }
         }
+
     }
 }
-
-
