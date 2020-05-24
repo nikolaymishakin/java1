5a15ea39b5df3711121e4a2b57dab9c31775f4b9
Николай <nikolaymishakin@yandex.ru>
2020-05-24 16:46:19 +0300

    ru.progwards.java1.lessons.basics

--- src/ru/progwards/java1/lessons/arrays/ArraySort.java
+++ src/ru/progwards/java1/lessons/arrays/ArraySort.java
@@ -4,7 +4,7 @@ public class ArraySort {
     public static void sort(int[] a){
         int tmp;
         for(int i = 0;i<a.length;i++){
-            for(int j = i+1;j<a.length;j++){
+            for(int j = 1;j<a.length;j++){
                 if(a[i]>a[j]){
                     tmp = a[i];
                     a[i] = a[j];
