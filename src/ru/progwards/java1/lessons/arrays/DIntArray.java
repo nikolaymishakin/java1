9a89090ecb4d3455e686e94b34a16c31fe70f032
Николай <nikolaymishakin@yandex.ru>
2020-05-30 18:41:28 +0300

    ru.progwards.java1.lessons.basics

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -1,29 +1,26 @@
 package ru.progwards.java1.lessons.arrays;
 
-import java.util.Arrays;
+        import java.util.Arrays;
 
 public class DIntArray {
-    private int[] a = new int[2];
-DIntArray () {
-}
+    private int[] a = new int[] {0,1,2};
+    DIntArray () {
+    }
     public void add(int num) {
-    int[] b = new int[3];
-    b = Arrays.copyOf(b,a.length+num);
- }
+        int[] b = Arrays.copyOf(a,a.length+num);
+    }
 
     public void atInsert(int pos, int num){
-    int [] c = new int[3];
-    Arrays.copyOf(c,a.length+num);
-    pos = num;
+        int [] c = Arrays.copyOf(a,a.length+1);
+        c[pos] = num;
     }
 
     public void atDelete(int pos){
-    int [] d = new int[1];
-    Arrays.copyOf(d,a.length-pos);
+        int [] d = Arrays.copyOf(a,a.length-pos);
     }
 
     public int at(int pos){
-    return pos;
+        return pos;
     }
 
     public static void main(String[] args) {
