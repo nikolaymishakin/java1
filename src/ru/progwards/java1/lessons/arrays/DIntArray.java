398da1676d772faccf2f4a742a1eba5a8dff4246
Николай <nikolaymishakin@yandex.ru>
2020-06-13 15:07:14 +0300

    ru.progwards.java1.lessons.basics

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -3,7 +3,7 @@ package ru.progwards.java1.lessons.arrays;
         import java.util.Arrays;
 
 public class DIntArray {
-    private int[] a = {2,58,36,9};
+    private int[] a;
 
     DIntArray() {
     }
