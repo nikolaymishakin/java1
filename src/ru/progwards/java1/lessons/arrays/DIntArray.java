1f014557c84eabc7c88a39aa1e36df726dbbae9f
Николай <nikolaymishakin@yandex.ru>
2020-09-05 12:19:32 +0300

    ru.progwards.java1.lessons.basics

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -3,32 +3,28 @@ package ru.progwards.java1.lessons.arrays;
         import java.util.Arrays;
 
 public class DIntArray {
-    private int[] a = new int[0];
+    private int[] a;
     DIntArray() {
+
     }
     public void add(int num) {
-            int[] b = new int[a.length];
+       int [] b = new int[a.length];
             b = Arrays.copyOf(a,a.length);
             a = new int[a.length + 1];
-            a = Arrays.copyOf(b,b.length + 1);
-            a[a.length - 1] = num;
+            b = Arrays.copyOf(a,a.length + 1);
+            b[a.length - 1] = num;
         }
 
-        public void atInsert ( int pos, int num){
-            int[] c = new int[a.length - pos];
-            for(int i = 0;i < c.length; i++){
-                c[i] = a[pos + 1];
-            }
-          int[]  b = new int[a.length];
-            b= Arrays.copyOf(a,a.length);
-            a = Arrays.copyOf(a,a.length + 1);
-            for(int i = 0;i < pos - 1;i ++){
-                a[i] = b[i];
-            }
-            a[pos - 1] = num;
-            for(int i = pos; i < a.length;i ++){
-                a[i] = b[i - 1];
-            }
+        public void atInsert ( int pos, int num) {
+        int [] c = new int[a.length];
+            c = Arrays.copyOf(a,a.length);
+            a = new int[a.length + 1];
+            for (int i = 0; i <pos; i++)
+            a[i] = c[i];
+            a[pos] = num;
+            for (int i = pos+1; i < a.length+1; i++)
+                a[i] = c[i-1];
+
         }
 
         public void atDelete ( int pos){
@@ -42,17 +38,16 @@ public class DIntArray {
 
         }
 
-        public int at ( int pos){
-            return a[pos];
+        public int at (int pos){
+            return pos;
         }
 
         public static void main (String[]args){
             int num = 3, pos = 2;
-            int[] a = {};
+            int[] a = {0,5,-8,10,12};
             System.out.println(Arrays.toString(a));
             System.out.println();
             int[] d = new int[a.length + 1];
-            System.arraycopy(a, 0, d, 0, a.length);
             d[a.length - 1] = num;
             int[] b = new int[a.length + 1];
             System.arraycopy(a, 0, b, 0, a.length);
