5bff81503ac4492f7c112b9c093685fae13651af
Николай <nikolaymishakin@yandex.ru>
2020-09-05 19:01:12 +0300

    ru.progwards.java1.lessons.basics

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -10,9 +10,9 @@ public class DIntArray {
     }
     public void add(int num) {
         if(a.length==0) System.out.println("a is null");
-       a = new int[a.length+1];
-       a = Arrays.copyOf(a,a.length+1);
-       a[a.length-1] = num;
+       int [] b = new int[a.length+1];
+       b = Arrays.copyOf(a,a.length+1);
+       b[a.length-1] = num;
         }
 
         public void atInsert ( int pos, int num) {
