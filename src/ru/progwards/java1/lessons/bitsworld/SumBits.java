715c3af6e099987f8ada619d2d60fcc75ea56d7e
Николай <nikolaymishakin@yandex.ru>
2020-09-26 16:59:42 +0300

    Revert "ru.progwards.java1.lessons.basics"
    This reverts commit ca4a9ab9

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -3,7 +3,7 @@ package ru.progwards.java1.lessons.bitsworld;
 public class SumBits {
     public static int sumBits(byte value){
         int summ = 0;
-        while(value > 0) summ+= (int)(value&1>>>1);
+        while(value >= 0) summ+= (int)(value&1>>>1);
         return summ;
     }
 
