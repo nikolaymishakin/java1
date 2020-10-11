3394f3bd2d41c6e15c7b0f1f0b6e9b46487376ae
Николай <nikolaymishakin@yandex.ru>
2020-10-11 20:32:51 +0300

    Revert "ru.progwards.java1.lessons.basics"
    This reverts commit ca4a9ab9

--- src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
+++ src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
@@ -4,12 +4,12 @@ public class CheckBit {
     public static int checkBit(byte value, int bitNumber){
         int value1 = value;
         if(value1<0) value1=value&0b0000_0000_0000_0000_0000_0000_1111_1111;
-       return value1&(1<<bitNumber);
+       return value1&(1<<bitNumber-1);
     }
 
     public static void main(String[] args) {
-        byte value = (byte)0b10011001;
-        int bitNumber = 3;
+        byte value = (byte)-21;
+        int bitNumber = 1;
         System.out.println(Integer.toBinaryString(value));
         System.out.println(checkBit(value,bitNumber));
     }
