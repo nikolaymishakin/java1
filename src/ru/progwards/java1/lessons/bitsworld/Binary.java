332291dc1906a842e7772d2ef6d6d21360e84f86
Николай <nikolaymishakin@yandex.ru>
2020-10-17 12:49:36 +0300

    Revert "ru.progwards.java1.lessons.basics"
    This reverts commit ca4a9ab9

--- src/ru/progwards/java1/lessons/bitsworld/Binary.java
+++ src/ru/progwards/java1/lessons/bitsworld/Binary.java
@@ -2,12 +2,16 @@ package ru.progwards.java1.lessons.bitsworld;
 
 public class Binary {
     private byte num;
-    public Binary(byte num){
-        this.num=num;
+
+    public Binary(byte num) {
+        this.num = num;
+
         }
-    public String toString(){
-        return String.format("%32s", Integer.toBinaryString(num).replace(' ','0'));
+
+    public String toString() {
+        return String.format("%8s", Integer.toBinaryString(num).replace(' ', '0'));
     }
+
     public static void main(String[] args) {
 
     }
