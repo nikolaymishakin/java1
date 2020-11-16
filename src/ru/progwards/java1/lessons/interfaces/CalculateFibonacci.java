134d0c9b5de5d0cf45a65c81cce26a336be357d0
Николай <nikolaymishakin@yandex.ru>
2020-11-17 00:27:17 +0300

    Revert "ru.progwards.java1.lessons.basics"
    This reverts commit ca4a9ab9

--- src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
+++ src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
@@ -7,7 +7,7 @@ public class CalculateFibonacci {
         int a = 0, b = 1, c;
         if (n == 0) return a;
         for (int i = 2; i <= n; i++) {
-            if ( lastFibo.n != lastFibo.fibo) lastFibo.fibo = b;
+            if ( i != lastFibo.n) lastFibo.fibo = b;
                 c = a + b;
                 a = b;
                 b = c;
