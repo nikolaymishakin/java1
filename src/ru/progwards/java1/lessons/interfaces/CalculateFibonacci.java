ca0a36a8ceb5e13d1980c286beea11eb8cb970b2
Николай <nikolaymishakin@yandex.ru>
2020-12-13 14:34:32 +0300

    Revert "Revert "ru.progwards.java1.lessons.basics""
    This reverts commit 5b71a493

--- src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
+++ src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
@@ -4,15 +4,12 @@ public class CalculateFibonacci {
     private static CacheInfo lastFibo;
 
     public static int fiboNumber (int n) {
-        CacheInfo cacheInfo = new CacheInfo();
-        cacheInfo.n = n;
-        cacheInfo.fibo = 0;
         int a = 0, b = 1, c;
-            for (int i = 2; i <= cacheInfo.n; i++) {
+            for (int i = 2; i <= n; i++) {
                 c = a + b;
                 a = b;
                 b = c;
-                if (b != cacheInfo.n) cacheInfo.fibo = b;
+                if (b != n) getLastFibo().fibo = getLastFibo().n;
             }
             return b;
         }
@@ -20,6 +17,10 @@ public class CalculateFibonacci {
     public static class CacheInfo {
         public int n;
         public int fibo;
+        CacheInfo(int n, int fibo){
+            this.n = n;
+            this.fibo = fibo;
+        }
     }
 
     public static CacheInfo getLastFibo() {
@@ -30,7 +31,8 @@ public class CalculateFibonacci {
     }
 
     public static void main(String[] args) {
-        System.out.println(fiboNumber(10));
+        CacheInfo cacheInfo = new CacheInfo(1,0);
+        System.out.println(cacheInfo.n);
     }
 
 }
