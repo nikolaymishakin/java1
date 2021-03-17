e70ac6531b83ce19e99c8e91670cd0bcf663b799
Николай <nikolaymishakin@yandex.ru>
2021-03-17 17:58:09 +0300

    Revert "Revert "ru.progwards.java1.lessons.basics""
    This reverts commit 5b71a493

--- src/ru/progwards/java1/lessons/bigints/AbsInteger.java
+++ src/ru/progwards/java1/lessons/bigints/AbsInteger.java
@@ -2,13 +2,17 @@ package ru.progwards.java1.lessons.bigints;
 
 public abstract class AbsInteger {
     public abstract int getValue();
+
     static AbsInteger add(AbsInteger num1, AbsInteger num2) {
         int res = num1.getValue() + num2.getValue();
-        if (num1 instanceof ByteInteger || num2 instanceof ByteInteger )
-            return new ByteInteger((byte) res);
-        else if (num1 instanceof ShortInteger || num2 instanceof ShortInteger)
+        if (res <= Byte.MAX_VALUE && res >= Byte.MIN_VALUE)
+            return new ByteInteger((byte)res);
+        else if (res <= Short.MAX_VALUE && res >=Short.MIN_VALUE)
             return new ShortInteger((short) res);
         else return new IntInteger(res);
     }
 
+    public static void main(String[] args) {
+
+    }
 }
